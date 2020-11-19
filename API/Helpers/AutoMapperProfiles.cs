556d791a5b4733b769b0a6f4fbd566ad1b115744
Davor Bulić <davor.bulic@eonsystem.com>
2020-11-19 17:01:36 +0100

    End of section 12

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -7,13 +7,14 @@ using AutoMapper;
 namespace API.Helpers
 {
     public class AutoMapperProfiles : Profile
-    {
+{
         public AutoMapperProfiles()
         {
             CreateMap<AppUser, MemberDto>()
             .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
             .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotoDto>();
+            CreateMap<RegisterDto, AppUser>();
 
             CreateMap<MemberUpdateDto, AppUser>();
         }
